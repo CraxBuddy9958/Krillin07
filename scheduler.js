dc1de1db521816bab37ac6c297d7de67256afe4e
CraxBuddy9958 <arifrah119560@gmail.com>
2026-04-16 11:09:55 +0530

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -3,7 +3,7 @@
 // ============================================
 
 // ⚡⚡⚡ SET YOUR LAUNCH TIME HERE (IST) ⚡⚡⚡
-const LAUNCH_HOUR_IST = 10;      // 23 = 11 PM
+const LAUNCH_HOUR_IST = 13;      // 23 = 11 PM
 const LAUNCH_MINUTE_IST = 03;    // 27 minutes = 11:27 PM IST
 
 // ============================================
